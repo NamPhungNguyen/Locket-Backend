3c0365ddadafa10354a3995600dcb1e9d541991c
NamPhungNguyen <namhien12082003@gmail.com>
2025-07-17 22:46:53 +0700

    remove

--- functions/userFunction/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const router = require('./routes');
-
-const app = express();
-app.use(bodyParser.json()); // Parse JSON bodies in the request
-app.use('/users', router); // Mount the user routes under /users
-
-module.exports = app; // Export Express app cho handler dùng
